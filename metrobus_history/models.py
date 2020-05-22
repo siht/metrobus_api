aae11d8f11de837043f22d5aca3ac7f0a6ff0c0c
siht <carlos.tz.00.00@gmail.com>
2020-05-22 02:30:06 -0500

    first model approach

--- metrobus_history/models.py
+++ metrobus_history/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+
+class District(models.Model):
+    name = models.CharField(max_length=15)
+
+
+class Place(models.Model):
+    latitude = models.IntegerField()
+    longitude = models.IntegerField()
+    district = models.ForeignKey('District', on_delete=models.CASCADE)
+
+    class Meta:
+        unique_together = ('latitude', 'longitude',)
+
+
+class Metrobus(models.Model):
+    serie = models.CharField(max_length=5)
+    where_i_was = models.ManyToManyField(Place, through='HistoricalPoint')
+
+
+class HistoricalPoint(models.Model):
+    metrobus = models.ForeignKey(Metrobus, on_delete=models.CASCADE)
+    place = models.ForeignKey(Place, on_delete=models.CASCADE)
+    date = models.DateField()
