08ea9a22c4aa3a84f341361c85df66acc3603a3d
siht <carlos.tz.00.00@gmail.com>
2020-06-02 01:49:14 -0500

    add metrobus to grepql endpoints

--- metrobus_history/schema.py
+++ metrobus_history/schema.py
@@ -2,7 +2,10 @@ import graphene
 
 from graphene_django.types import DjangoObjectType
 
-from .models import District
+from .models import (
+    District,
+    Metrobus,
+)
 
 
 class DistrictType(DjangoObjectType):
@@ -10,8 +13,17 @@ class DistrictType(DjangoObjectType):
         model = District
 
 
+class MetrobusType(DjangoObjectType):
+    class Meta:
+        model = Metrobus
+
+
 class Query(object):
     all_districts = graphene.List(DistrictType)
+    all_metrobuses = graphene.List(MetrobusType)
 
     def resolve_all_districts(self, info, **kwargs):
         return District.objects.all()
+
+    def resolve_all_metrobuses(self, info, **kwargs):
+        return Metrobus.objects.all()
